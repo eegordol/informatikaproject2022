aa859c99f76d3be240128c76a61c450d8add2702
eegordol <egor.dolidze@gmeil.com>
2022-11-28 22:49:29 +0300

    initial commit

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.dolidze;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
