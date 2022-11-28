875041a24d96f899227cb9c5f70ed99d066b8826
eegordol <egor.dolidze@gmeil.com>
2022-11-28 23:45:07 +0300

    window works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,13 +1,26 @@
 package app;
 
 
-import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
+    private final Window window;
+
+    public Application() {
+        window = App.makeWindow();
+        window.setEventListener(this);
+        window.setVisible(true);
+    }
+
+
     @Override
     public void accept(Event event) {
-
+        if (event instanceof EventWindowClose) {
+            App.terminate();
+        } else if (event instanceof EventWindowCloseRequest) {
+            window.close();
+        }
     }
 }
