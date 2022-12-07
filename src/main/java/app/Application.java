7bea13387b11328098be38e07cf7fbdb865bcc9f
eegordol <egor.dolidze@gmeil.com>
2022-12-07 21:52:32 +0300

    canvas works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,8 @@ package app;
 
 
 import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Surface;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -18,10 +20,32 @@ public class Application implements Consumer<Event> {
         window.setTitle("Java 2D");
         window.setWindowSize(900, 900);
         window.setWindowPosition(100, 100);
+
         switch (Platform.CURRENT) {
             case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
             case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
         }
+
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+
+        // перебираем слои
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        // если окну не присвоен ни один из слоёв
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
+
         window.setVisible(true);
 
     }
@@ -29,6 +53,7 @@ public class Application implements Consumer<Event> {
 
     /**
      * Обработчик событий
+     *
      * @param event событие
      */
     @Override
@@ -37,6 +62,11 @@ public class Application implements Consumer<Event> {
             App.terminate();
         } else if (event instanceof EventWindowCloseRequest) {
             window.close();
+        } else if (event instanceof EventFrameSkija ee) {
+            // получаем поверхность рисования
+            Surface s = ee.getSurface();
+            // очищаем её канвас заданным цветом
+            s.getCanvas().clear(0xFF264653);
         }
     }
 }
